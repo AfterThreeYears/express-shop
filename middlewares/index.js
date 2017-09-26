221ec3262926991b3e4c39ccb8adb1a8de3772ee
AfterThreeYears <4703@qq.com>
2017-09-26 13:07:06 +0800

    test

--- middlewares/index.js
+++ middlewares/index.js
@@ -9,7 +9,6 @@ const {corsConfig} = require('../config');
 
 module.exports = (app) => {
     
-    // view engine setup
     // app.set('views', path.join(__dirname, 'views'));
     // app.set('view engine', 'jade');
     // app.engine('html', ejs.__express);
